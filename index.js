f8475c32403bd55f13146ded72e124d1f421e555
Kate <kate.dubitski@gmail.com>
2018-01-16 19:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObject;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+//function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
-}
+//}
